88132a560e7673ef8685577c83d72b8532a0adb0
Brian Snyder <briansnyder@Brians-MacBook-Pro-4.local>
2025-04-19 21:53:30 -0700

    Fix base path for Vercel vs GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Detect GitHub Pages vs Vercel
+const isProd = process.env.NODE_ENV === 'production';
+const isVercel = process.env.VERCEL === '1';
 
 export default defineConfig({
-  base: '/DecentralizeGame/', // 👈 this is the key!
   plugins: [react()],
-})
+  base: isProd && !isVercel ? '/DecentralizeGame/' : '/',
+});
