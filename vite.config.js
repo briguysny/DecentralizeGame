0d463d8f4c200d4f4d4097cbbedb228c470255e9
Brian Snyder <briansnyder@Brians-MacBook-Pro-4.local>
2025-04-19 21:55:31 -0700

    Support both GitHub Pages and Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Detect GitHub Pages vs Vercel
 const isProd = process.env.NODE_ENV === 'production';
 const isVercel = process.env.VERCEL === '1';
 
